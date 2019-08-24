b1fffceb38879bcf398cb40379ee6f9cbb5467e7
Geesa-Vihara <geesavihara97@gmail.com>
2019-08-24 13:44:10 +0530

    20190824-01 create login file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.jsx";
+import Login from "layouts/Login.jsx";
 import RTL from "layouts/RTL.jsx";
 
 import "assets/css/material-dashboard-react.css?v=1.7.0";
@@ -15,6 +16,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/admin" component={Admin} />
+      <Route path="/login" component={Login} />
       <Route path="/rtl" component={RTL} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
