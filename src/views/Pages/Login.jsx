6a98ce266b0357f07e513ee79ef232cfdef66341
Geesa-Vihara <geesavihara97@gmail.com>
2019-09-15 16:27:23 +0530

    20190915-03 login page adjustments

--- src/views/Pages/Login.jsx
+++ src/views/Pages/Login.jsx
@@ -2,71 +2,91 @@ import React, {Component} from "react";
 import avatar from "assets/img/faces/lavish.jpg";
 import "css/Login.css";
 import Footer from "components/Footer/Footer";
-import GridItem from "components/Grid/GridItem.jsx";
-import CustomInput from "components/CustomInput/CustomInput.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
 import { Link } from "react-router-dom";
 
 class Login extends Component{
-    
+
+    constructor() {
+        super();
+        this.state = {
+          username: "",
+          password: "",
+          errors: {}
+        };        
+      }
+      onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+      };
+      onSubmit = e => {
+        e.preventDefault();const userData = {
+          username: this.state.username,
+          password: this.state.password
+        };
+        console.log(userData);
+      };
+
     render(){
-        
+        const { errors } = this.state;
         return(
-            <div>
-            <div className="login">
-                <div className="logo">                    
-                    <img id="logoimg" src={avatar} alt="img" />                    
-                </div>                
-                <h6 className="userlogin"><b>User Log In</b></h6>
-               
-                <GridContainer justify="center">
-                    <div id="logintitle">
-                        <label >Welcome to Lavish Tea Pvt LTD<br />
-                        <small >Sales Force Automation System</small></label>
-                    </div>
-                    <GridItem xs={8} sm={8} md={8}>
-                        <CustomInput
-                            labelText="Username"
-                            id="username"
-                            formControlProps={{
-                            fullWidth: true
-                            }}
-                        />
-                    </GridItem>
-                    <GridItem xs={8} sm={8} md={8}>
-                        <CustomInput
-                            color="info"
-                            labelText="Password"
-                            id="password"
-                            formControlProps={{
-                            fullWidth: true
-                            }}
-                            inputProps={{
-                                type: "password",
-                                required: true,
+            <div style={{ marginTop: "8rem" }}>
+                <div className="container">
+                    <div className="row">
+                        <div className="logo" >                    
+                            <img id="logoimg" src={avatar} alt="img" />                    
+                        </div>                
+                    <h6 className="userlogin"><b>User Log In</b></h6>
+                        <div className="col s8 offset-s2" >                
+                            <div className="col s12" style={{ textAlign: "center" }}>
+                                <label >Welcome to Lavish Tea Pvt LTD<br />
+                                <small >Sales Force Automation System</small>
+                                </label>                
+                            </div>
+                            <form noValidate onSubmit={this.onSubmit}>
+                                <div className="input-field col s12">
+                                    <input
+                                    onChange={this.onChange}
+                                    value={this.state.username}
+                                    error={errors.username}
+                                    id="username"
+                                    type="text"
+                                    />
+                                    <label htmlFor="username">Username</label>
+                                </div>
+                                <div className="input-field col s12">
+                                    <input
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    id="password"
+                                    type="password"
+                                    />
+                                    <label htmlFor="password">Password</label>
+                                </div>
                                 
-                            }}
-                        />
-                    </GridItem>
-                    <GridItem xs={8} sm={8} md={8}>                   
-                        <label id="remember"><input type="checkbox"/> Keep me signed in</label>
-                    </GridItem>
-                
-                    <GridItem xs={8} sm={8} md={8} >
-                        <Button id="login" color="info">LOGIN</Button> 
-                        <div id="logintitle">
-                            <label id="forgot"><small >Forgot your password? <Link to="/forgotpassword">Get help</Link></small></label>                     
-                        </div>  
-                    </GridItem>
-                    
-                </GridContainer>
-                
-            </div>
-            
-            <div className="bottom">
-                <Footer />
-            </div>
+                                <div className="col s12" >                                    
+                                    <button
+                                        style={{
+                                        width: "100%",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"                                        
+                                        }}
+                                        type="submit"
+                                        className="btn btn-large waves-effect waves-light hoverable info accent-3"
+                                        >
+                                        Login
+                                    </button>
+                                    <div className="col s12" style={{ textAlign: "center" }}>
+                                        <label><small >Forgot your password? <Link to="/forgotpassword">Get help</Link></small></label>                     
+                                    </div>  
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <Footer />
+                </div>
             </div>
          
         )
