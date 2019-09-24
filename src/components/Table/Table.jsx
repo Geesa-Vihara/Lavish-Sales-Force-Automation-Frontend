4b0a24b7e7fbc2cb29178f51c420bb2aa50cd2a8
codekcg23 <kavigamage23@gmail.com>
2019-09-25 00:12:35 +0530

    20190925-01 salesRep table frontend few changes

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -9,6 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import Paper from '@material-ui/core/Paper';
+//icons
 import IconButton from "@material-ui/core/IconButton";
 import GetAppIcon from '@material-ui/icons/GetApp';
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -23,11 +24,28 @@ import AddIcon from '@material-ui/icons/Add';
 const useStyles = makeStyles(theme => ({              //styles for add button
   fab: {
     margin: theme.spacing(1),
+    backgroundColor: "#018786"
   },
   extendedIcon: {
     marginRight: theme.spacing(1),
   },
+  icon:{
+    color:"#018786"
+  },
+  table: {
+    marginBottom: "0",
+    width: "100%",
+    maxWidth: "100%",
+    backgroundColor: "transparent",
+    borderSpacing: "0",
+    borderCollapse: "collapse"
+},
+tableHeadCell: {
+  color: "inherit",
+  fontSize: "1.1em"
   
+},
+
 }));
 
 function CustomTable({ ...props }) {
@@ -39,16 +57,16 @@ function CustomTable({ ...props }) {
   return (
     <div> 
       <Paper>   
-      <Fab color="primary" aria-label="add"  className={classes.fab}>         { /*Add button to add salesreps*/}
+      <Fab aria-label="add"  className={classes.fab}>         { /*Add button to add salesreps*/}
         <AddIcon />
       </Fab>                                        {/*className={classes.tableResponsive}*/}
-      <Table >                                       {/* className={classes.table}*/}
+      <Table className={classes.table}>                                       {/* className={classes.table}*/}
         {tableHead !== undefined ? (
           <TableHead >                               {/* className={classes[tableHeaderColor + "TableHeader"]}*/}
             <TableRow >                              {/* className={classes.tableHeadRow}*/}
               {tableHead.map((prop, key) => {
                 return (
-                  <TableCell key={key}>              {/* className={classes.tableCell + " " + classes.tableHeadCell} */}
+                  <TableCell key={key} className={classes.tableHeadCell}>              {/* className={classes.tableCell + " " + classes.tableHeadCell} */}
                     {prop}
                   </TableCell>
                 );
@@ -68,14 +86,14 @@ function CustomTable({ ...props }) {
                   );
                 })}
                 <TableCell>  
-                    <IconButton  aria-label="view" color="primary">
-                        <ViewIcon />
+                    <IconButton  aria-label="view" >
+                        <ViewIcon className={classes.icon}/>
                     </IconButton>       
-                    <IconButton  aria-label="edit" color="primary">
-                        <EditIcon />
+                    <IconButton  aria-label="edit" >
+                        <EditIcon className={classes.icon} />
                     </IconButton>
-                    <IconButton  aria-label="delete" color="primary">
-                        <DeleteIcon />
+                    <IconButton  aria-label="delete">
+                        <DeleteIcon className={classes.icon}/>
                     </IconButton>
                 </TableCell>
               </TableRow>
@@ -83,7 +101,7 @@ function CustomTable({ ...props }) {
           })}
         </TableBody>
       </Table>
-      <Fab variant="extended" size="medium" color="primary" aria-label="export"  className={classes.fab}>         { /*Add button to add salesreps*/}
+      <Fab variant="extended" size="medium"  aria-label="export"  className={classes.fab}>         { /*Add button to add salesreps*/}
         <GetAppIcon className={classes.extendedIcon}/>
         Export
       </Fab>  
