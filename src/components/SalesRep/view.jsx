a7ffe6b7de12751a269c8f7842840bd4cfa76d67
codekcg23 <kavigamage23@gmail.com>
2019-09-27 00:24:29 +0530

    20190927-02 salesRep crud

---
+++ src/components/SalesRep/view.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class View extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Edit Component!!</p>
+            </div>
+        )
+    }
+}
