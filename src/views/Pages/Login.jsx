be4fdf4154f3e9a1c27ec1ae41dbc340cbfcaf7e
Geesa-Vihara <geesavihara97@gmail.com>
2019-09-29 16:48:58 +0530

    20170929-02 login design change

--- src/views/Pages/Login.jsx
+++ src/views/Pages/Login.jsx
@@ -1,9 +1,80 @@
 import React, {Component} from "react";
 import avatar from "assets/img/faces/lavish.jpg";
-import "css/Login.css";
 import Footer from "components/Footer/Footer";
 import { Link } from "react-router-dom";
 import axios from "axios"
+import Button from '@material-ui/core/Button';
+import { withStyles,createMuiTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { ThemeProvider } from '@material-ui/styles';
+import { teal} from '@material-ui/core/colors';
+
+const useStyles = theme => ({
+    container:{
+        margin: " -50px auto 0",
+        color: "rgba(0, 0, 0, 0.87)",
+        backgroundColor: "rgb(255, 255, 255)",
+        width: "450px",
+        height: "600px",
+        borderRadius: "6px",
+        boxShadow: "0 1px 4px 0 rgba(0, 0, 0, 0.14)"
+    },
+    logo:{
+        margin: "-50px auto 0",
+        overflow: "hidden",
+        maxWidth: "180px",
+        boxShadow: "0 16px 38px -12px rgba(0, 0, 0, 0.56), 0 4px 25px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
+        maxHeight: "180px",
+        borderRadius: "50%"
+    },
+    logoimg:{
+        width: "100%",
+        height: "100%"
+    },
+    textcontainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginBottom:30
+      },
+      textField: {
+        marginLeft: theme.spacing(10),        
+        width: 300,     
+      },
+      textfielderror: {
+        marginLeft: theme.spacing(10),        
+        color:"red"
+      },
+      heading:{
+        textAlign:"center",
+        marginBottom:10
+      },
+      headingerror:{
+        textAlign:"center",
+        marginBottom:10,
+        color:"red"
+    },
+      userlogin:{
+        fontSize:17,
+        color: "#2bbbad"
+      },
+      button:{
+        marginLeft: theme.spacing(10),
+        width: 300,
+        height:45,
+        borderRadius: "3px",
+        letterSpacing: "1.5px",
+        marginTop: "1rem"     
+      },
+      forgot:{
+        textAlign: "center" 
+      }, 
+         
+  });
+  const theme = createMuiTheme({
+    palette: {
+      primary: teal,
+    },
+  });
 
 class Login extends Component{
 
@@ -49,75 +120,67 @@ class Login extends Component{
 
     render(){
         const { errors } = this.state;
+        const { classes } = this.props;
         return(
-            <div style={{ marginTop: "8rem" }}>
-                <div className="container">
-                    <div className="row">
-                        <div className="logo" >                    
-                            <img id="logoimg" src={avatar} alt="img" />                    
-                        </div>                
-                    <h6 className="userlogin"><b>User Log In</b></h6>
-                        <div className="col s8 offset-s2" >                
-                            <div className="col s12" style={{ textAlign: "center" }}>
-                                <label >Welcome to Lavish Tea Pvt LTD<br />
-                                <small >Sales Force Automation System</small>
-                                </label>                
-                            </div>
-                            <form noValidate onSubmit={this.onSubmit}>
-                                <div className="col s12"> 
-                                    <span className="red-text">
-                                            {errors.incorrect}                                        
-                                    </span>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input
-                                    onChange={this.onChange}
-                                    value={this.state.username}
-                                    error={errors.username}
-                                    id="username"
-                                    type="text"
-                                    />
-                                    <label htmlFor="username">Username</label>
-                                    <span className="red-text">
-                                        {errors.username}                                        
-                                    </span>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input
-                                    onChange={this.onChange}
-                                    value={this.state.password}
-                                    error={errors.password}
-                                    id="password"
-                                    type="password"
-                                    />
-                                    <label htmlFor="password">Password</label>
-                                    <span className="red-text">
-                                        {errors.password}                                        
-                                    </span>
-                                </div>
-                                
-                                <div className="col s12" >                                    
-                                    <button
-                                        style={{
-                                        width: "100%",
-                                        borderRadius: "3px",
-                                        letterSpacing: "1.5px",
-                                        marginTop: "1rem"                                        
-                                        }}
-                                        type="submit"
-                                        className="btn btn-large waves-effect waves-light hoverable info accent-3"
-                                        >
-                                        Login
-                                    </button>
-                                    <div className="col s12" style={{ textAlign: "center" }}>
-                                        <label><small >Forgot your password? <Link to="/forgotpassword">Get help</Link></small></label>                     
-                                    </div>  
-                                </div>
-                            </form>
+            <div style={{ marginTop: "8rem"}}>
+                <div className={classes.container}>
+                    <div className={classes.logo}>                    
+                        <img className={classes.logoimg} src={avatar} alt="img" />                    
+                    </div> 
+                    <div className={classes.heading}>             
+                        <p className={classes.userlogin}><b>USER LOGIN</b></p>
+                        <label >Welcome to Lavish Tea Pvt LTD<br />
+                            <small >Sales Force Automation System</small>
+                        </label>  
+                    </div> 
+                    <form noValidate onSubmit={this.onSubmit}>
+                        <div className={classes.headingerror}> 
+                            <span>
+                                {errors.incorrect}                                     
+                            </span>
                         </div>
-                    </div>
+                        <div className={classes.textcontainer}>                                
+                            <TextField
+                                id="username"
+                                label="Username"
+                                onChange={this.onChange}
+                                value={this.state.username}                              
+                                type="text"
+                                className={classes.textField}                                
+                            />
+                            <span className={classes.textfielderror}>
+                                {errors.username}                                        
+                            </span> 
+                        </div>
+                        <div className={classes.textcontainer}>
+                            <TextField
+                                id="password"
+                                label="Password"
+                                onChange={this.onChange}
+                                value={this.state.password} 
+                                type="password"
+                                className={classes.textField}
+                            />
+                            <span className={classes.textfielderror}>
+                                {errors.password}                                        
+                            </span>
+                        </div>
+                        <ThemeProvider theme={theme}>                           
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                className={classes.button}
+                                >
+                                Login
+                            </Button>
+                        </ThemeProvider>
+                        <div className={classes.forgot}>
+                            <label><small >Forgot your password? <Link to="/forgotpassword">Get help</Link></small></label>                     
+                        </div> 
+                    </form>
                 </div>
-                <div className="bottom">
+                <div>
                     <Footer />
                 </div>
             </div>
@@ -127,4 +190,4 @@ class Login extends Component{
 
 }
 
-export default Login;
+export default withStyles(useStyles)(Login);
