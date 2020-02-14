e315e605d8714a816aa3c4511fc948b55717d0d3
Geesa-Vihara <geesavihara97@gmail.com>
2020-02-14 18:35:32 +0530

    20200214- reports change

--- src/views/Reports/Reports.jsx
+++ src/views/Reports/Reports.jsx
@@ -6,7 +6,7 @@ import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { ThemeProvider } from '@material-ui/styles';
-import { Paper,Card, Modal, CardContent } from '@material-ui/core';
+import { Paper,Card, CardContent } from '@material-ui/core';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -17,26 +17,37 @@ import Select from '@material-ui/core/Select';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
 import SalesReport from '../Reports/salesreport.jsx';
-import StockBalance from '../Reports/stockbalance.jsx';
-import DistOutstanding from '../Reports/distoutstanding.jsx';
-import { Icon } from "@material-ui/core";
+import StockBalance from '../Reports/stockbalance';
 
+import Axios from 'axios';
+import { Table,TableBody,TableCell,TableHead,TableRow }  from "@material-ui/core";
 import {
     MuiPickersUtilsProvider,
     KeyboardDatePicker,
   } from '@material-ui/pickers';
-
+import Assignment from "@material-ui/icons/Assignment";
+import axios from 'axios';
+import  { Link,Route}  from 'react-router-dom';
 
   
 const useStyles = theme => ({
+  table:{
+    textAlign:"center"
+  },
+  tableHeadCell:{
+    textAlign:"center"
+  },
+  tableContent:{
+    textAlign:"center"
+  },
   root: {
     width: '90%',
     marginTop:"2%"
   },  
   buttonprint:{
     backgroundColor:"#DCDCDC",
-    float:"right",
-    marginBottom:10
+    marginBottom:10,
+    float:"right"
 },
   modal:{
     display: 'flex',
@@ -59,11 +70,6 @@ const useStyles = theme => ({
   },
   nextfinish:{
     marginRight: theme.spacing(1),
-
-    //backgroundColor:"#1b5e20",
-    /* '&:hover':{
-      backgroundColor:"#8EB69B",
-    }, */
     color:"black"
   },
   instructions: {
@@ -85,19 +91,46 @@ const body = createMuiTheme({
     });
 
 class Reports extends Component {
-  state={
+  constructor(props){
+  super(props);
+  this.state={
     activeStep:0,
     report:"0",
-    selectedDate:Date.now(),
-    selectedDatefrom:Date.now(),
-    selectedDateto:Date.now()+1000*60*60*24*31,
+    selectedDate:Date.now(), 
+    distributors:[],
     dist:"0",
     selectdist:"0",
-    open:false
+    checkStatus:0,
+    sales:[],
   }
-
-  handleNext = () => {  
+  
+  this.handleNext=this.handleNext.bind(this);
+  this.updateStatus=this.updateStatus.bind(this);
+}
+  componentDidMount(){
+    var token = localStorage.getItem('jwtToken');
+    Axios
+        .get('/distributors',{
+        headers:{
+            'Authorization':token
+        }
+        })
+        .then(res => {
+        this.setState({
+          distributors : res.data
+        });
+        })
+        .catch(err => {
+        if(err.tokenmessage){
+            this.setState({isexpire:true});
+        }
+        })  
+  }
+  handleNext() {  
     this.setState({activeStep:this.state.activeStep + 1});
+    if(this.state.activeStep === this.getSteps().length - 1 ){
+        this.updateStatus()
+    }
   };
 
   handleBack = () => {
@@ -111,13 +144,11 @@ class Reports extends Component {
   handleReset = () => {
     this.setState({activeStep:0});
     this.setState({report:"0"});
-    this.setState({selectedDatefrom:Date.now()});
-    this.setState({selectedDateto:Date.now()+1000*60*60*24*31});
     this.setState({selectedDate:Date.now()});
     this.setState({dist:"0"});
     this.setState({selectdist:"0"});
-
-
+    this.setState({checkStatus:0});
+    this.setState({sales:[]});
 
   };
   handleChange=(e)=>{
@@ -133,38 +164,36 @@ class Reports extends Component {
     this.setState({selectedDate:date})
 
   }
-  handleDateChangefrom=(date)=>{
-    this.setState({selectedDatefrom:date});
-    this.setState({selectedDateto:date})
+updateStatus(){
+  var token = localStorage.getItem('jwtToken');
+        
+  const userData={
+      distributor:this.state.dist==="0"?"All":this.state.distributors[this.state.selectdist].userName,
+      date:this.state.selectedDate,
+      type:this.state.report==="0"?"sales":"stock",
 
-  }
-  handleDateChangeto=(date)=>{
-    this.setState({selectedDateto:date})
-
-  }
-  enablemodel=()=>{
-      this.setState({open:true})
-  }
-  disablemodel=()=>{
-    this.setState({open:false})
-}
-generateReport=(report)=>{
-  switch (report){
-    case "0":
-      return(        
-        <SalesReport/>
-      );
-    case "1":
-      return(
-        <StockBalance/>
-      );
-    case "2":
-      return(
-        <DistOutstanding/>
-      );
-    default :
-        return "Could not generate report"; 
-  }
+    };
+    axios.post('/reports/generateRep',userData,{
+      headers:{
+        "Authorization": token 
+        }
+    })
+      .then(res=>{
+        if(res.data.length!==0){
+        this.setState({
+            checkStatus:1,
+            sales:res.data
+      
+      })
+    }
+}               
+      )
+      .catch(err=>{
+            
+            if(err.tokenmessage){
+                this.setState({isexpire:true}) ; 
+            }
+        })
 }
   getStepContent=(step)=>{
     switch (step) {
@@ -175,14 +204,12 @@ generateReport=(report)=>{
                     <RadioGroup aria-label="report" name="reports" value={this.state.report} onChange={this.handleChange}>
                         <FormControlLabel value="0" control={<Radio color="primary" />} label="Sales Report" />
                         <FormControlLabel value="1" control={<Radio color="primary" />} label="Distributor Balance Stock Report" />
-                        <FormControlLabel value="2" control={<Radio color="primary"/>} label="Distributor Market Outstanding Report" />                    
                     </RadioGroup>
                 </FormControl>
             </ThemeProvider>
         );
       case 1:
-        if(this.state.report==="1"){
-        return (
+          return (
             <ThemeProvider theme={body}>  
                 <MuiPickersUtilsProvider utils={DateFnsUtils} >             
                         <KeyboardDatePicker
@@ -201,46 +228,11 @@ generateReport=(report)=>{
                 </MuiPickersUtilsProvider>
             </ThemeProvider>
         );
-        }else{
-            return (
-                <ThemeProvider theme={body}>  
-                    <MuiPickersUtilsProvider utils={DateFnsUtils} >             
-                            <KeyboardDatePicker
-                                style={{marginRight:"10%"}}
-                                variant="inline"
-                                format="MM-dd-yyyy"
-                                margin="normal"
-                                id="date-picker-inline"
-                                label="Select Date from"
-                                value={this.state.selectedDatefrom}
-                                onChange={this.handleDateChangefrom}
-                                KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                                }}
-                                
-                            /> 
-                            <KeyboardDatePicker
-                                minDate={this.state.selectedDatefrom}
-                                variant="inline"
-                                format="MM-dd-yyyy"
-                                margin="normal"
-                                id="date-picker-inline"
-                                label="Select Date to"
-                                value={this.state.selectedDateto}
-                                onChange={this.handleDateChangeto}
-                                KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                                }}
-                                
-                            />                                       
-                    </MuiPickersUtilsProvider>
-                </ThemeProvider>
-                );
-        }
       case 2:
         return (
         <ThemeProvider theme={body}>
             <FormControl component="fieldset" >
+              {}
                 <RadioGroup aria-label="distributor" name="distributors" value={this.state.dist} onChange={this.handleDistChange}>
                     <FormControlLabel value="0" control={<Radio color="primary" />} label="All" />
                     <FormControlLabel value="1" control={<Radio color="primary" />} label="Choose" />
@@ -252,11 +244,9 @@ generateReport=(report)=>{
                                 onChange={this.handleChangeselectdist}   
                                 style={{textAlign:"left"}}                
                             >
-                                <MenuItem value={0}>Kamal perera</MenuItem> 
-                                <MenuItem value={1}>Nuwan chamara</MenuItem> 
-                                <MenuItem value={2}>Lasantha dilshan</MenuItem> 
-                                <MenuItem value={3}>Hashan senanayake</MenuItem> 
-                                <MenuItem value={4}>Gayan sampath</MenuItem> 
+                                {this.state.distributors.map((distributors,i)=>
+                                    <MenuItem key={distributors._id} value={i}>{distributors.fullName}</MenuItem> 
+                                )}
                             </Select>
                         </FormControl> :""
                     }       
@@ -273,16 +263,10 @@ generateReport=(report)=>{
         return 'Unknown step';
     }
   }
-  printreport=()=>{
-    const content = document.getElementById('reporttoprint').innerHTML;   
-    const orderHtml = ' <html><head><title></title><style>table {line-height:1;}</style></head><body>' + content + '</body></html>';
-    document.body.innerHTML = orderHtml;        
-    window.location.reload();
-    window.print();  
-}
+ 
   render(){
     const {classes} = this.props;
-    const {activeStep,report} = this.state;
+    const {activeStep,sales} = this.state;
     const steps=this.getSteps();
   return (
     <div className={classes.root}>
@@ -305,24 +289,122 @@ generateReport=(report)=>{
         {activeStep === steps.length ? (
           <div>
             <Typography className={classes.instructions}>
-            <b>All steps completed. Click 'Generate report' to view the report</b>
+            <b>Click 'View Report' to view the report and print or Click 'Reset' to start again</b>
             </Typography>
             <Button onClick={this.handleReset} className={classes.button}>
               Reset
             </Button>
-            <Button className={classes.nextfinish} onClick={this.enablemodel}>
-              Generate report
-            </Button>
-             <Modal className={classes.modal} onClose={this.disablemodel}open={this.state.open}>
-              <Card className={classes.card}>
-                <CardContent className={classes.cardcontainer}>
-                    <div>
-                      <Button className={classes.buttonprint} onClick={this.printreport} ><Icon style={{fontSize:40}}>printer</Icon>Print</Button>
-                    </div> 
-                  {this.generateReport(report)} 
+            <Card className={classes.card}>
+                <CardContent className={classes.cardcontainer}> 
+                {this.state.report==="0"?(
+                  <Table className={classes.table}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell className={classes.tableHeadCell}>
+                          Report No
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Distributor Username
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Salesrep Fullname
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Report
+                        </TableCell> 
+                      </TableRow>
+                    </TableHead>
+                    {this.state.checkStatus===1?                   
+                    <TableBody>                             
+                    {Object.keys(sales).map((_,i) => {  
+                      return(                                     
+                      <TableRow key={i} hover>
+                        <TableCell className={classes.tableContent}>{i}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i]._id.distUsername}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i]._id.repName}</TableCell>
+                        <TableCell className={classes.tableContent}>
+                        <Link to={`/admin/reports/sales/${sales[i]._id.distUsername}`}>  
+                          <Button
+                            variant="contained"
+                            className={classes.button}
+                            startIcon={<Assignment />}                    
+                          >
+                            View report
+                          </Button>  
+                          </Link>                                     
+                      </TableCell>
+                    </TableRow>
+                    
+                  )})} 
+                         
+              </TableBody>:
+              <TableBody>
+                 <TableRow hover>
+                        <TableCell className={classes.tableContent} colSpan="4">No data to display</TableCell>                        
+                    </TableRow>
+              </TableBody>}
+              </Table>
+                ):(
+                  <Table className={classes.table}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell className={classes.tableHeadCell}>
+                          Stock No
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Distributor Username
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Salesrep Fullname
+                        </TableCell>
+                        <TableCell className={classes.tableHeadCell}>
+                          Date
+                        </TableCell> 
+                        <TableCell className={classes.tableHeadCell}>
+                          Time
+                        </TableCell> 
+                        <TableCell className={classes.tableHeadCell}>
+                          Report
+                        </TableCell> 
+                      </TableRow>
+                    </TableHead>
+                    {this.state.checkStatus===1?                   
+                    <TableBody>                             
+                    {Object.keys(sales).map((_,i) => {  
+                      return(                                     
+                      <TableRow key={i} hover>
+                        <TableCell className={classes.tableContent}>{sales[i].stockno}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i]._id.distname}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i].repname}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i].currdate}</TableCell>
+                        <TableCell className={classes.tableContent}>{sales[i].hour===24?`12:00 AM`:sales[i].hour===12 && sales[i].minutes===0?`12:00 PM`:sales[i].hour>12 ?`${sales[i].hour-12}: ${sales[i].minutes} PM`:`${sales[i].hour}:${sales[i].minutes} AM`}</TableCell>
+                        <TableCell className={classes.tableContent}>
+                        <Link to={`/admin/reports/stock/${sales[i]._id.distname}`}>  
+                          <Button
+                            variant="contained"
+                            className={classes.button}
+                            startIcon={<Assignment />}                    
+                          >
+                            View report
+                          </Button>  
+                          </Link>                                     
+                      </TableCell>
+                    </TableRow>
+                    
+                  )})} 
+                         
+              </TableBody>:
+              <TableBody>
+                 <TableRow hover>
+                        <TableCell className={classes.tableContent} colSpan="4">No data to display</TableCell>                        
+                    </TableRow>
+              </TableBody>}
+              </Table>
+                )
+                
+                }
                 </CardContent>
-              </Card>       
-            </Modal> 
+              </Card>
           </div>
         ) : (
           <div>
@@ -340,12 +422,18 @@ generateReport=(report)=>{
               >
                 {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
               </Button>
+              
             </div>
             
           </div>
         )}
       </div>
+      <Route exact path='/admin/reports/sales/:id2' render={(props) => (
+                <SalesReport {...props} date={this.state.selectedDate} /> )}  /> 
+      <Route exact path='/admin/reports/stock/:id2' render={(props) => (
+      <StockBalance {...props} date={this.state.selectedDate} /> )}  /> 
     </div>
+    
   );
 }
 }
